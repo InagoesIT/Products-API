c34e597e19e10b85b3dd48c0dce51380ac0e54e7
ina <wenulife@gmail.com>
2023-12-18 16:10:40 +0100

    added handwritten yaml open api documentation

--- Program.cs
+++ Program.cs
@@ -1,11 +1,13 @@
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+        {
+            c.SwaggerDoc("v1", new OpenApiInfo { Title = "Products API", Version = "v1" });
+        });
 
 var app = builder.Build();
 
@@ -13,7 +15,10 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Products API V1");
+            });
 }
 
 app.UseHttpsRedirection();
