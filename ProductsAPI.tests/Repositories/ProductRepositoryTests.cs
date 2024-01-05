21e4604a84ff44e1d555e705406364c53d2a1bc6
ina <wenulife@gmail.com>
2024-01-05 19:22:01 +0100

    used database context in testing the repo

--- ProductsAPI.tests/Repositories/ProductRepositoryTests.cs
+++ ProductsAPI.tests/Repositories/ProductRepositoryTests.cs
@@ -1,8 +1,15 @@
+using Moq;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
 using ProductsAPI.Entities;
 using ProductsAPI.Repositories;
 using ProductsAPI.Helpers;
+using ProductsAPI.Data;
 
 namespace ProductsAPI.Tests.Repositories;
+
+// * mocking inspiration: https://learn.microsoft.com/en-us/ef/ef6/fundamentals/testing/mocking
 
 public class ProductRepositoryTests
 {
@@ -10,17 +17,48 @@ public class ProductRepositoryTests
     public void When_AddProduct_Then_ShouldReturnSuccess()
     {
         // * Arrange
-        (string name, float price) sut = CreateSUT();
-        ResultOfEntity<Product> result = Product.Create(sut.name, sut.price);
+        DatabaseContext context = new DatabaseContext("test");
+        context.products.RemoveRange(context.products);
+        context.SaveChanges();
+
+        (string name, float price) sut = CreateProductSUT();
+        ResultOfEntity<Product> productWrapper = Product.Create(sut.name, sut.price);
+
+        ProductRepository repository = new ProductRepository(context);
 
         // * Act
+        Result result = repository.Add(productWrapper.Entity);
 
-
+        // * Assert
+        result.IsSuccess.Should().BeTrue();
     }
 
-    private (string name, float price) CreateSUT()
+    [Fact]
+    public void When_AddProductWithExistentName_Then_ShouldReturnFailure()
+    {
+        // * Arrange
+        DatabaseContext context = new DatabaseContext("test");
+        context.products.RemoveRange(context.products);
+        context.SaveChanges();
+
+        (string name, float price) sut = CreateProductSUT();
+        ResultOfEntity<Product> productWrapper = Product.Create(sut.name, sut.price);
+
+        ProductRepository repository = new ProductRepository(context);
+        repository.Add(productWrapper.Entity);
+
+        // * Act
+        Result result = repository.Add(productWrapper.Entity);
+
+        // * Assert
+        result.IsSuccess.Should().BeFalse();
+    }
+
+    private (string name, float price) CreateProductSUT()
     {
         (string name, float price) result = ("Apple", 0.1f);
         return result;
     }
+
+
 }
--- ProductsAPI/Data/DatabaseContext.cs
+++ ProductsAPI/Data/DatabaseContext.cs
@@ -5,16 +5,25 @@ namespace ProductsAPI.Data;
 
 public class DatabaseContext : DbContext
 {
-    public DbSet<Product> products { get; set; }
+    private static string DEFAULT_DATABASE_NAME = "Products";
+    private readonly string databaseName;
+    public virtual DbSet<Product> products { get; set; }
 
     public DatabaseContext()
     {
+        this.databaseName = DEFAULT_DATABASE_NAME;
+        this.Database.EnsureCreated();
+    }
+
+    public DatabaseContext(string databaseName)
+    {
+        this.databaseName = databaseName;
         this.Database.EnsureCreated();
     }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
         // * connection string builder, helps with sql server
-        optionsBuilder.UseSqlite("Data Source = Products.db");
+        optionsBuilder.UseSqlite($"Data Source = {this.databaseName}.db");
     }
 }
